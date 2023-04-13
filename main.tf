5ff9ab8c00724194a69535bc4a2c930df4cd7e73
wellmadeoldfashioned <chrisray@pm.me>
2023-04-13 08:35:47 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "us-west-2" 
+}
+
+resource "aws_instance" "splunk_SH" {
+  ami           = "ami-0b5cb59327b8d7e1f" # us-west-2 AMI for Splunk
+  instance_type = "t3.micro" # Update to t3.large in prod
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.splunk_server.id}"]
+  subnet_id              = "${aws_subnet.splunk_server.id}"
+ }
+
+
+
+resource "aws_security_group" "splunk_server" {
+  name_prefix = "splunk-group"
+  
+  ingress {
+    from_port   = 22 # ssh port
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["24.247.202.81/32"]
+  }
+
+  ingress {
+    from_port   = 8000 # Web UI port
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["24.247.202.81/32"]
+  }
+
+  ingress {
+    from_port   = 8088 # HEC port
+    to_port     = 8088
+    protocol    = "tcp"
+    cidr_blocks = ["24.247.202.81/32"]
+  }
+
+  ingress {
+    from_port   = 9997 # Forwarder port
+    to_port     = 9997
+    protocol    = "tcp"
+    cidr_blocks = ["24.247.202.81/32"]
+  }
+  
+  tags = {
+    Name = "splunk-server"
+  }
+}
+
+resource "aws_subnet" "splunk_server" {
+  vpc_id = "vpc-002c2876364b2b282"
+  cidr_block = "172.31.1.0/28" 
+}
+
+output "splunk_server_public_ip" {
+  value = "${aws_instance.splunk_SH.public_ip}"
+  description = "The public IP of the Splunk server"
+}
