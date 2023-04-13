e3e7fdbe957040b46d8282856772415c499f9c6f
wellmadeoldfashioned <chrisray@pm.me>
2023-04-13 08:42:26 -0400

    updated arch to include all EC2

--- main.tf
+++ main.tf
@@ -1,16 +1,60 @@
 provider "aws" {
   region = "us-west-2" 
 }
-
 resource "aws_instance" "splunk_SH" {
   ami           = "ami-0b5cb59327b8d7e1f" # us-west-2 AMI for Splunk
   instance_type = "t3.micro" # Update to t3.large in prod
   associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.splunk_server.id}"]
   subnet_id              = "${aws_subnet.splunk_server.id}"
- }
+ 
+  #provisioner "local-exec" {
+  # source="path\to\script\script.sh" #install app
+  # destination="/tmp/script.sh" #AMI destination for install
+  # command = "/bin/bash /tmp/script.sh" #executes script on destination
+}
 
+  
+  tags = {
+    Name = "splunk-server"
+  }
 
+resource "aws_instance" "splunk_IDX1" {
+  ami           = "ami-0b5cb59327b8d7e1f" # us-west-2 AMI for Splunk
+  instance_type = "t3.micro" # Update to t3.large in prod
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.splunk_server.id}"]
+  subnet_id              = "${aws_subnet.splunk_server.id}"
+  
+  tags = {
+    Name = "splunk-server"
+  }
+}
+
+resource "aws_instance" "splunk_IDX2" {
+  ami           = "ami-0b5cb59327b8d7e1f" # us-west-2 AMI for Splunk
+  instance_type = "t3.micro" # Update to t3.large in prod
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.splunk_server.id}"]
+  subnet_id              = "${aws_subnet.splunk_server.id}"
+  
+  tags = {
+    Name = "splunk-server"
+  }
+}
+
+resource "aws_instance" "splunk_mgmt" {
+  ami           = "ami-0b5cb59327b8d7e1f" # us-west-2 AMI for Splunk
+  instance_type = "t3.micro" # Update to t3.medium in prod
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.splunk_server.id}"]
+  subnet_id              = "${aws_subnet.splunk_server.id}"
+  
+  tags = {
+    Name = "splunk-server"
+  }
+
+}
 
 resource "aws_security_group" "splunk_server" {
   name_prefix = "splunk-group"
